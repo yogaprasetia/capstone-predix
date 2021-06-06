d9c163f60cf8d3a3ade15499c7e52e287aeb589b
rizkyhamdana07 <rizky.hamdana07@gmail.com>
2021-06-06 22:57:30 +0800

    Finishing

--- app/src/main/java/com/predixteam/predixai/utils/DataDummy.kt
+++ app/src/main/java/com/predixteam/predixai/utils/DataDummy.kt
@@ -11,7 +11,7 @@ object DataDummy {
                 "Model 01",
                 "Pneumonia",
                 "Pneumonia is an inflammatory condition of the lung primarily affecting the small air sacs known as alveoli. Symptoms typically include some combination of productive or dry cough, chest pain, fever and difficulty breathing. The severity of the condition is variable",
-                "file:///android_asset/models/placeholder2.jpg",
+                "file:///android_asset/models/pneumonia.jpg",
                 "pneumonia.txt"
             )
         )
@@ -20,9 +20,9 @@ object DataDummy {
             ModelEntity(
                 2,
                 "Model 02",
-                "TBC",
+                "Tuberculosis",
                 "Tuberculosis (TB) is an infectious disease usually caused by Mycobacterium tuberculosis (MTB) bacteria. Tuberculosis generally affects the lungs, but can also affect other parts of the body. Most infections show no symptoms, in which case it is known as latent tuberculosis. About 10% of latent infections progress to active disease which, if left untreated, kills about half of those affected.",
-                "file:///android_asset/models/placeholder2.jpg",
+                "file:///android_asset/models/tuberculosis.jpg",
             "tbc.txt"
             )
         )
@@ -33,7 +33,7 @@ object DataDummy {
                 "Model 03",
                 "Covid 19",
                 "Coronavirus disease 2019 (COVID-19) is a contagious disease caused by severe acute respiratory syndrome coronavirus 2 (SARS-CoV-2). The first known case was identified in Wuhan, China in December 2019. The disease has since spread worldwide, leading to an ongoing pandemic.",
-                "file:///android_asset/models/placeholder2.jpg",
+                "file:///android_asset/models/covid19.jpg",
             "covid.txt"
             )
         )
